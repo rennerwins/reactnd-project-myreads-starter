cd15fefc280a2beba6e3994ce4a10eeca75c41a4
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2017-07-17 23:31:12 +0700

    Decompose BookCover to stateless component

--- src/Book.js
+++ src/Book.js
@@ -1,20 +1,13 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const Book = props => {
-  const { book } = props
+	const { book } = props
 	return (
 		<li>
 			<div className="book">
 				<div className="book-top">
-					<div
-						className="book-cover"
-						style={{
-							width: 128,
-							height: 193,
-							backgroundImage:
-								'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")'
-						}}
-					/>
+					<BookCover image={book.imageLinks.thumbnail} />
 					<div className="book-shelf-changer">
 						<select>
 							<option value="none" disabled>
@@ -27,11 +20,37 @@ const Book = props => {
 						</select>
 					</div>
 				</div>
-				<div className="book-title">{book.title}</div>
-				<div className="book-authors">{book.authors[0]}</div>
+				<div className="book-title">
+					{book.title}
+				</div>
+				<div className="book-authors">
+					{book.authors[0]}
+				</div>
 			</div>
 		</li>
 	)
+}
+
+Book.propTypes = {
+	book: PropTypes.object.isRequired
+}
+
+const BookCover = props => {
+	const { image } = props
+	return (
+		<div
+			className="book-cover"
+			style={{
+				width: 128,
+				height: 193,
+				backgroundImage: `url(${image})`
+			}}
+		/>
+	)
+}
+
+BookCover.propTypes = {
+	image: PropTypes.string.isRequired
 }
 
 export default Book
