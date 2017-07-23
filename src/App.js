edde3aed330563235577e6b53d375daa5cf4dfab
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2017-07-23 14:04:06 +0700

    Refactor: List books state and change shelf function up to parent components

--- src/App.js
+++ src/App.js
@@ -3,13 +3,39 @@ import Search from './Search'
 import ListBooks from './ListBooks'
 import { Route } from 'react-router-dom'
 import './App.css'
+import * as BooksAPI from './utils/BooksAPI'
 
 class BooksApp extends React.Component {
+	state = {
+		books: []
+	}
+
+	componentDidMount() {
+		this.getAllBook()
+	}
+
+	getAllBook = async () => {
+		const res = await BooksAPI.getAll()
+		await this.setState({ books: res })
+	}
+
+	changeBookShelf = async (book, shelf) => {
+		await BooksAPI.update(book, shelf)
+		this.getAllBook()
+	}
+
 	render() {
 		return (
 			<div className="app">
-				<Route exact path="/" component={ListBooks} />
-				<Route path="/search" component={Search} />
+				<Route
+					exact
+					path="/"
+					render={() => <ListBooks books={this.state.books} change={this.changeBookShelf} />}
+				/>
+				<Route
+					path="/search"
+					render={() => <Search books={this.state.books} change={this.changeBookShelf} />}
+				/>
 			</div>
 		)
 	}
